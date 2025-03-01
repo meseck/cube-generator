a0e57ed3e4c48db261ab3197f81b8028da90fa8c
Erik Meseck <git@meseck.dev>
2025-03-01 20:58:50 +0100

    feat: render hard-coded 3D array of cubes

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,94 @@
 import {
   IsometricCanvas,
+  IsometricGroup,
   IsometricRectangle,
+  IsometricRectangleProps,
   PlaneView,
 } from "@elchininet/isometric";
 import "./App.css";
 import { useEffect, useRef } from "react";
 
-function draw(canvas: IsometricCanvas) {
-  const commonProps = { height: 1, width: 1 };
+type ColorPalette = {
+  base: string;
+  lightShade: string;
+  darkShade: string;
+};
 
-  const top = new IsometricRectangle({
-    ...commonProps,
-    id: "cube-top",
-    planeView: PlaneView.TOP,
-  });
-  const right = new IsometricRectangle({
-    ...commonProps,
-    id: "cube-right",
-    className: "right-piece",
-    planeView: PlaneView.FRONT,
-  });
-  const left = new IsometricRectangle({
-    ...commonProps,
-    id: "cube-left",
-    planeView: PlaneView.SIDE,
-  });
+function draw(canvas: IsometricCanvas, colorPalette: ColorPalette) {
+  function drawCube(x: number, y: number, z: number) {
+    const commonProps: Omit<IsometricRectangleProps, "planeView"> = {
+      height: 1,
+      width: 1,
+      strokeColor: colorPalette.darkShade,
+      strokeWidth: 6,
+      // fillOpacity: 0.5,
+    };
 
-  top.top = 1;
-  right.right = 1;
-  left.left = 1;
+    const top = new IsometricRectangle({
+      id: "cube-top",
+      top: 1,
+      fillColor: colorPalette.base,
+      planeView: PlaneView.TOP,
+      ...commonProps,
+    });
+    const right = new IsometricRectangle({
+      id: "cube-right",
+      right: 1,
+      fillColor: colorPalette.lightShade,
+      planeView: PlaneView.FRONT,
+      ...commonProps,
+    });
+    const left = new IsometricRectangle({
+      id: "cube-left",
+      left: 1,
+      fillColor: colorPalette.darkShade,
+      planeView: PlaneView.SIDE,
+      ...commonProps,
+    });
 
-  canvas
-    .addChild(top)
-    .addChild(right)
-    .addChild(left);
+    const cube = new IsometricGroup({ top: z, right: x, left: y });
+    cube.addChildren(top, right, left);
+
+    canvas.addChild(cube);
+  }
+  // Bottom layer
+  drawCube(0, 0, 0);
+  drawCube(1, 0, 0);
+  drawCube(2, 0, 0);
+  //
+  drawCube(0, 1, 0);
+  drawCube(1, 1, 0);
+  drawCube(2, 1, 0);
+  //
+  drawCube(0, 2, 0);
+  drawCube(1, 2, 0);
+  drawCube(2, 2, 0);
+
+  // Middle layer
+  drawCube(0, 0, 1);
+  drawCube(1, 0, 1);
+  drawCube(2, 0, 1);
+  //
+  drawCube(0, 1, 1);
+  drawCube(1, 1, 1);
+  drawCube(2, 1, 1);
+  //
+  drawCube(0, 2, 1);
+  drawCube(1, 2, 1);
+  drawCube(2, 2, 1);
+
+  // Top layer
+  drawCube(0, 0, 2);
+  drawCube(1, 0, 2);
+  drawCube(2, 0, 2);
+  //
+  drawCube(0, 1, 2);
+  drawCube(1, 1, 2);
+  drawCube(2, 1, 2);
+  //
+  drawCube(0, 2, 2);
+  drawCube(1, 2, 2);
+  drawCube(2, 2, 2);
 }
 
 function App() {
@@ -55,12 +110,18 @@ function App() {
       canvas = new IsometricCanvas({
         container: ref.current,
         backgroundColor: "#00000000",
-        scale: 120,
-        width: 500,
-        height: 320,
+        scale: 100,
+        width: 800,
+        height: 800,
       });
 
-      draw(canvas);
+      const monoColorPalette = {
+        base: "#FFFFFF",
+        lightShade: "#FFFFFF",
+        darkShade: "#343434",
+      };
+
+      draw(canvas, monoColorPalette);
     }
 
     return () => {
@@ -70,7 +131,7 @@ function App() {
 
   return (
     <>
-      <div ref={ref} />
+      <div id="canvas-wrapper" ref={ref} />
       <button type="button" onClick={handleClick}>
         Save SVG
       </button>
