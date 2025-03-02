b595dcf2b4184c9f74abf91fc717777c30fdce5d
Erik Meseck <git@meseck.dev>
2025-03-02 12:41:45 +0100

    feat: render 3D array of cubes programmatically

--- src/App.tsx
+++ src/App.tsx
@@ -45,44 +45,14 @@ function draw(canvas: IsometricCanvas, colorPalette: ColorPalette) {
 
     canvas.addChild(cube);
   }
-  // Bottom layer
-  drawCube(0, 0, 0);
-  drawCube(1, 0, 0);
-  drawCube(2, 0, 0);
-  //
-  drawCube(0, 1, 0);
-  drawCube(1, 1, 0);
-  drawCube(2, 1, 0);
-  //
-  drawCube(0, 2, 0);
-  drawCube(1, 2, 0);
-  drawCube(2, 2, 0);
 
-  // Middle layer
-  drawCube(0, 0, 1);
-  drawCube(1, 0, 1);
-  drawCube(2, 0, 1);
-  //
-  drawCube(0, 1, 1);
-  drawCube(1, 1, 1);
-  drawCube(2, 1, 1);
-  //
-  drawCube(0, 2, 1);
-  drawCube(1, 2, 1);
-  drawCube(2, 2, 1);
-
-  // Top layer
-  drawCube(0, 0, 2);
-  drawCube(1, 0, 2);
-  drawCube(2, 0, 2);
-  //
-  drawCube(0, 1, 2);
-  drawCube(1, 1, 2);
-  drawCube(2, 1, 2);
-  //
-  drawCube(0, 2, 2);
-  drawCube(1, 2, 2);
-  drawCube(2, 2, 2);
+  for (let x = 0; x < 3; x++) {
+    for (let y = 0; y < 3; y++) {
+      for (let z = 0; z < 3; z++) {
+        drawCube(x, y, z);
+      }
+    }
+  }
 }
 
 function App() {
